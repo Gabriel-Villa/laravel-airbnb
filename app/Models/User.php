3740e52561379879227a1887317121ddb8cec2a1
Gabriel-Villa <VillaCapurroGabriel@gmail.com>
2023-04-06 13:47:50 -0500

    add Vitest

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
