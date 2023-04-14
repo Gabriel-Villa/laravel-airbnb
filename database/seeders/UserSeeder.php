683a61eba3534823fd70a987507cd61559f1dae2
Gabriel-Villa <VillaCapurroGabriel@gmail.com>
2023-04-14 16:40:38 -0500

    Change n° counts in UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,7 +12,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(100)->create();
+        User::factory()->count(10)->create();
         User::factory()->create(['email' => 'dev@gmail.com']);
     }
 }
